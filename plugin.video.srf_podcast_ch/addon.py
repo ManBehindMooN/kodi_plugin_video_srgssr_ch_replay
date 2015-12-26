ce97beea180e606366caec22fdd87419c7259b0a
Christian Rueedi <christian.rueedi@gmail.com>
2015-12-27 00:08:07 +0100

    Added function to page to older episodes of a show. Unfortunately, there is a bug if episodes and folders are mixed in a list, the episode icons don't show up anymore.

--- plugin.video.srf_podcast_ch/addon.py
+++ plugin.video.srf_podcast_ch/addon.py
@@ -35,116 +35,121 @@ viewModeShows = str(addon.getSetting("viewIDShows"))
 
 #'this method list all TV shows from SRF when SRF-Podcast was selected in the main menu'
 def listTvShows():
-    url = 'http://il.srf.ch/integrationlayer/1.0/ue/srf/tv/assetGroup/editorialPlayerAlphabetical.json'
-    response = json.load(open_srf_url(url))
-    shows =  response["AssetGroups"]["Show"]
-    title = ''
-    desc = ''
-    picture = ''
-    for show in shows:
-        try:
-            title = show['title']
-        except:
-            title = 'No Title'
-        try:
-            desc = show['description']
-        except:
-            desc = 'No Description'
-        try:
-            picture = show['Image']['ImageRepresentations']['ImageRepresentation'][0]['url']
-        except:
-            picture = ''
-        addShow(title, show['id'], 'listEpisodes', desc, picture)
-        
-    xbmcplugin.addSortMethod(pluginhandle,1)
-    xbmcplugin.endOfDirectory(pluginhandle)
-    if forceViewMode:
-        xbmc.executebuiltin('Container.SetViewMode('+viewModeShows+')')
+	url = 'http://il.srf.ch/integrationlayer/1.0/ue/srf/tv/assetGroup/editorialPlayerAlphabetical.json'
+	response = json.load(open_srf_url(url))
+	shows =  response["AssetGroups"]["Show"]
+	title = ''
+	desc = ''
+	picture = ''
+	page = 1
+	mode = 'listEpisodes'
+	for show in shows:
+		try:
+			title = show['title']
+		except:
+			title = 'No Title'
+		try:
+			desc = show['description']
+		except:
+			desc = 'No Description'
+		try:
+			picture = show['Image']['ImageRepresentations']['ImageRepresentation'][0]['url']
+		except:
+			picture = ''
+		addShow(title, show['id'], mode, desc, picture,page)
+		
+	xbmcplugin.addSortMethod(pluginhandle,1)
+	xbmcplugin.endOfDirectory(pluginhandle)
+	if forceViewMode:
+		xbmc.executebuiltin('Container.SetViewMode('+viewModeShows+')')
 
 #'this method list all episodes of the selected show'
-def listEpisodes(showid,showbackground):
-    url = 'http://il.srf.ch/integrationlayer/1.0/ue/srf/assetSet/listByAssetGroup/'+showid+'.json'
-    response = json.load(open_srf_url(url))
-    show =  response["AssetSets"]["AssetSet"]
-    
-    for episode in show:
-        title = episode['title']
-        url = ''
-        desc = ''
-        picture = '' 
-        pubdate = episode['publishedDate']
-        
-        try:
-            desc = episode['Assets']['Video'][0]['AssetMetadatas']['AssetMetadata'][0]['description']
-        except:
-            desc = 'No Description'
-        try:
-            picture = episode['Assets']['Video'][0]['Image']['ImageRepresentations']['ImageRepresentation'][0]['url']
-        except:
-            # no picture
-            picture = ''
-        try:
-            length = int(episode['Assets']['Video'][0]['duration']) / 1000 / 60
-        except:
-            length = 0
-        try:
-            url = episode['Assets']['Video'][0]['id']
-        except:
-            url = 'no url'
+def listEpisodes(showid,showbackground,page):
+	url = 'http://il.srf.ch/integrationlayer/1.0/ue/srf/assetSet/listByAssetGroup/'+showid+'.json?pageNumber='+str(page)
+	response = json.load(open_srf_url(url))
+	maxpage = response["AssetSets"]["@maxPageNumber"]
+	show =  response["AssetSets"]["AssetSet"]
+		
+	for episode in show:
+		title = episode['title']
+		url = ''
+		desc = ''
+		picture = '' 
+		pubdate = episode['publishedDate']
+		
+		try:
+			desc = episode['Assets']['Video'][0]['AssetMetadatas']['AssetMetadata'][0]['description']
+		except:
+			desc = 'No Description'
+		try:
+			picture = episode['Assets']['Video'][0]['Image']['ImageRepresentations']['ImageRepresentation'][0]['url']
+		except:
+			# no picture
+			picture = ''
+		try:
+			length = int(episode['Assets']['Video'][0]['duration']) / 1000 / 60
+		except:
+			length = 0
+		try:
+			url = episode['Assets']['Video'][0]['id']
+		except:
+			url = 'no url'
+		addLink(title, url, 'playepisode', desc, picture, length, pubdate,showbackground)
 
-        addLink(title, url, 'playepisode', desc, picture, length, pubdate,showbackground)
-       
-    xbmcplugin.endOfDirectory(pluginhandle)
-    if forceViewMode:
-        xbmc.executebuiltin('Container.SetViewMode('+viewModeShows+')')
+	# check if another page is available
+	page = int(page)
+	maxpage = int(maxpage)
+	if page < maxpage:
+		page = page + 1
+		addnextpage('Show older episodes of this show..', showid, 'listEpisodes', '', '',page)
+	
+	xbmcplugin.endOfDirectory(pluginhandle)
+	if forceViewMode:
+		xbmc.executebuiltin('Container.SetViewMode('+viewModeShows+')')
     
-#'this method plays the selected episode'    
+	#'this method plays the selected episode'    
 def playepisode(episodeid):
-    
-    besturl = ''
-    downloadflag = 0
-    
-    try:
-        url = 'http://il.srf.ch/integrationlayer/1.0/ue/srf/video/play/'+episodeid+'.json'
-        response = json.load(open_srf_url(url))
-        urls =  response["Video"]["Playlists"]['Playlist'][@protocol='HTTP-HDS']['url']
-        besturl = ''
-        if urls.__len__() > 1:
-            for tempurl in urls:
-                if tempurl['@quality'] == 'HD':
-                    besturl = tempurl['text']
-        else:
-            besturl = urls[0]['text']
-            
-        downloadflag = 1
-    except:
-        print "not for download"
-    
-    if downloadflag == 0:
-        try:
-            url = 'http://il.srf.ch/integrationlayer/1.0/ue/srf/video/play/'+episodeid+'.json'
-            response = json.load(urllib2.urlopen(url))
-            urls =  response["Video"]["Playlists"]['Playlist'][1]['url']
-            besturl = ''
-            if urls.__len__() > 1:
-                for tempurl in urls:
-                    if tempurl['@quality'] == 'HD':
-                        besturl = tempurl['text']
-            else:
-                besturl = urls[0]['text']
-                
-            downloadflag = 0
-        except:
-            print "not for download"  
-    
-    
-    listitem = xbmcgui.ListItem(path=besturl)
-    xbmcplugin.setResolvedUrl(pluginhandle, True, listitem)
+	besturl = ''
+	downloadflag = 0
+	
+	try:
+		url = 'http://il.srf.ch/integrationlayer/1.0/ue/srf/video/play/'+episodeid+'.json'
+		response = json.load(open_srf_url(url))
+		urls =  response["Video"]["Playlists"]["Playlist"]["@protocol=HTTP-HDS"]["url"]
+		besturl = ''
+		if urls.__len__() > 1:
+			for tempurl in urls:
+				if tempurl['@quality'] == 'HD':
+					besturl = tempurl['text']
+		else:
+			besturl = urls[0]['text']
+		downloadflag = 1
+	except:
+		print "not for download"
+
+	if downloadflag == 0:
+		try:
+			url = 'http://il.srf.ch/integrationlayer/1.0/ue/srf/video/play/'+episodeid+'.json'
+			response = json.load(urllib2.urlopen(url))
+			urls =  response["Video"]["Playlists"]['Playlist'][1]['url']
+			besturl = ''
+			if urls.__len__() > 1:
+				for tempurl in urls:
+					if tempurl['@quality'] == 'HD':
+						besturl = tempurl['text']
+			else:
+				besturl = urls[0]['text']
+			
+			downloadflag = 0
+		except:
+			print "not for download"  
+	listitem = xbmcgui.ListItem(path=besturl)
+	xbmcplugin.setResolvedUrl(pluginhandle, True, listitem)
 
 #'helper method to create a folder with subitems'
-def addShow(name, url, mode, desc, iconimage):
+def addShow(name, url, mode, desc, iconimage,page):
     ok = True
-    directoryurl = sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&showbackground="+urllib.quote_plus(iconimage)
+    directoryurl = sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&showbackground="+urllib.quote_plus(iconimage)+"&page="+str(page)
     liz = xbmcgui.ListItem(name)
     liz.setIconImage("DefaultFolder.png")
     liz.setThumbnailImage(iconimage)
@@ -154,21 +159,35 @@ def addShow(name, url, mode, desc, iconimage):
     xbmcplugin.setContent(pluginhandle, 'tvshows')
     ok = xbmcplugin.addDirectoryItem(pluginhandle, url=directoryurl, listitem=liz, isFolder=True)
     return ok
+
+	#'helper method to create a folder with subitems'
+def addnextpage(name, url, mode, desc, iconimage,page):
+    ok = True
+    directoryurl = sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&showbackground="+urllib.quote_plus(iconimage)+"&page="+str(page)
+    liz = xbmcgui.ListItem(name)
+    #liz.setIconImage("DefaultFolder.png")
+    #liz.setThumbnailImage(iconimage)
+    liz.setLabel2(desc)
+    #liz.setArt({'poster' : iconimage , 'banner' : iconimage, 'fanart' : iconimage, 'clearart' : iconimage, 'clearlogo' : iconimage, 'landscape' : iconimage})
+    liz.setInfo(type="Video", infoLabels={"title": name, "plot": desc, "plotoutline": desc})
+    xbmcplugin.setContent(pluginhandle, 'tvshows')
+    ok = xbmcplugin.addDirectoryItem(pluginhandle, url=directoryurl, listitem=liz, isFolder=True)
+    return ok
     
 #'helper method to create an item in the list'
 def addLink(name, url, mode, desc, iconurl, length, pubdate, showbackground):
-    ok = True
-    linkurl = sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)
-    liz = xbmcgui.ListItem(name)
-    liz.setIconImage("DefaultFolder.png")
-    liz.setThumbnailImage(iconurl)
-    liz.setLabel2(desc)
-    liz.setArt({'poster' : iconurl , 'banner' : iconurl, 'fanart' : showbackground, 'clearart' : iconurl, 'clearlogo' : iconurl, 'landscape' : showbackground})
-    liz.setInfo(type='Video', infoLabels={"Title": name, "Duration": length, "Plot": desc, "Aired" : pubdate})
-    liz.setProperty('IsPlayable', 'true')
-    xbmcplugin.setContent(pluginhandle,"episodes")
-    ok = xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=linkurl, listitem=liz)
-    return ok
+	ok = True
+	linkurl = sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)
+	liz = xbmcgui.ListItem(name)
+	liz.setIconImage("DefaultFolder.png")
+	liz.setThumbnailImage(iconurl)
+	liz.setLabel2(desc)
+	liz.setArt({'poster' : iconurl , 'banner' : iconurl, 'fanart' : showbackground, 'clearart' : iconurl, 'clearlogo' : iconurl, 'landscape' : showbackground})
+	liz.setInfo(type='Video', infoLabels={"Title": name, "Duration": length, "Plot": desc, "Aired" : pubdate})
+	liz.setProperty('IsPlayable', 'true')
+	xbmcplugin.setContent(pluginhandle,"episodes")
+	ok = xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=linkurl, listitem=liz)
+	return ok
 
 #'helper method to retrieve parameters in a dict from the arguments given to this plugin by xbmc'
 def parameters_string_to_dict(parameters):
@@ -203,6 +222,7 @@ params = parameters_string_to_dict(sys.argv[2])
 mode = params.get('mode', '')
 url = params.get('url', '')
 showbackground = urllib.unquote_plus(params.get('showbackground', ''))
+page = params.get('page', '')
 
 #mode = urllib.unquote_plus(params.get('mode', ''))
 #url = urllib.unquote_plus(params.get('url', ''))
@@ -211,7 +231,7 @@ showbackground = urllib.unquote_plus(params.get('showbackground', ''))
 if mode == 'listTvShows':
     listTvShows()
 elif mode == 'listEpisodes':
-    listEpisodes(url,showbackground)
+    listEpisodes(url,showbackground,page)
 elif mode == 'playepisode':
     playepisode(url)
 else:
